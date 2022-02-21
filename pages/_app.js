48ece277adf0aa52339243a6a99f48d4dd14722f
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-02-21 16:57:39 +0530

    Fixing
    Temporary fix for NFT Buy method

--- pages/_app.js
+++ pages/_app.js
@@ -8,22 +8,22 @@ function MyApp({ Component, pageProps }) {
         <p className="text-4xl font-bold">Metaverse Market</p>
         <div className="flex mt-4">
           <Link href="/">
-            <a className="mr-4 text-pink-500">
+            <a className="mr-4 text-blue-500">
               Home
             </a>
           </Link>
           <Link href="/create-item">
-            <a className="mr-6 text-pink-500">
+            <a className="mr-6 text-blue-500">
               Sell Digital Asset
             </a>
           </Link>
           <Link href="/my-assets">
-            <a className="mr-6 text-pink-500">
+            <a className="mr-6 text-blue-500">
               My Digital Assets
             </a>
           </Link>
           <Link href="/creator-dashboard">
-            <a className="mr-6 text-pink-500">
+            <a className="mr-6 text-blue-500">
               Creator Dashboard
             </a>
           </Link>
--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,8 @@ export default function Home() {
   // state var to store the market
   const [Market, setMarket] = useState(null)
 
+  const [visible, setVisible] = useState(false)
+
   useEffect(() => {
     loadNFTs()
   }, [])
@@ -41,7 +43,6 @@ export default function Home() {
         name: meta.data.name,
         description: meta.data.description
       }
-      console.log(item);
       return item
     }))
     setNfts(items)
@@ -49,6 +50,8 @@ export default function Home() {
   }
 
   const buyNFT = async(nft) => {
+    setVisible(true)
+    return
     /* needs the user to sign the transaction, so will use Web3Provider and sign it */
     //Web3Modal to connect to any wallet present like metamask
     const web3Modal = new Web3Modal()
@@ -57,14 +60,17 @@ export default function Home() {
     const provider = new ethers.providers.Web3Provider(connection)
     //Getting the accounts using getSigner()
     const signer = provider.getSigner()
-    const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+    const contract = new ethers.Contract(marketaddress, MarketABI.abi, signer)
 
     /* user will be prompted to pay the asking proces to complete the transaction */
-    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')   
+
+    //Have to solve can't convert string to big number
+
+    /*const price = ethers.utils.parseEther(nft.price.toString())
     const transaction = await contract.createMarketSale(nftaddress, nft.tokenId, {
-      value: price
+      value: p
     })
-    await transaction.wait()
+    await transaction.wait()*/
     loadNFTs()
   }
 
@@ -77,6 +83,11 @@ export default function Home() {
   return (
     <div className="flex justify-center">
       <div className="px-4" style={{ maxWidth: '1600px' }}>
+        {
+          visible && (
+            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert"> The Buy button is under construction </div>
+          )
+        }
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
           {
             nfts.map((nft, i) =>(
@@ -90,7 +101,7 @@ export default function Home() {
                 </div>
                 <div className="p-4 bg-black">
                   <p className="text-2xl mb-4 font-bold text-white">{nft.price} ETH</p>
-                  <button className="w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
+                  <button className="w-full bg-blue-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNFT(nft)}>Buy</button>
                 </div>
               </div>
             ))
